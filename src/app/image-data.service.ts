7007569448eafb2e019dca49d133694f16d5e3f4
Marcus Jensen <marcusproman0507@gmail.com>
2023-12-19 12:28:55 +0100

    Cannot add same image twice now

--- src/app/image-data.service.ts
+++ src/app/image-data.service.ts
@@ -7,6 +7,12 @@ export class ImageDataService {
   imageData: any[] = [];
 
   addImageData(file: File): void {
-    this.imageData.push({ file });
+    // Check if the file is already in the array
+    const isFileAlreadyAdded = this.imageData.some(data => data.file.lastModified === file.lastModified && data.file.name === file.name && data.file.size === file.size && data.file.type === file.type);
+
+    // If the file is not already in the array, add it
+    if (!isFileAlreadyAdded) {
+      this.imageData.push({ file });
+    }
   }
 }
